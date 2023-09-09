8e398ecec610a6817be073c10931a800d67d69c0
teonadi <138980806+teonadi@users.noreply.github.com>
2023-09-09 16:46:12 +0400

    Add button clicks

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
+let allPlanetInfo;
+
 fetch("https://planets-api.vercel.app/api/v1/planets/")
   .then((res) => {
     return res.json();
   })
   .then((json) => {
-    fillInformation(json[1]);
+    allPlanetInfo = json;
   });
 
-function fillInformation(planetInfo) {
+let headInfo = document.getElementsByClassName("headInfo")[0];
+let overviewButtonHead = headInfo.children[0];
+let internalButtonHead = headInfo.children[1];
+let surfaceGeologyHead = headInfo.children[2];
+
+let bodyInfo = document.getElementsByClassName("bodyInfo")[0];
+let overviewButtonBody = bodyInfo.children[0];
+let internalButtonBody = bodyInfo.children[1];
+let surfaceGeologyBody = bodyInfo.children[2];
+
+overviewButtonBody.onclick = function () {
+  fillInformation(allPlanetInfo[0], "overview");
+};
+overviewButtonHead.onclick = function () {
+  fillInformation(allPlanetInfo[0], "overview");
+};
+
+internalButtonBody.onclick = function () {
+  fillInformation(allPlanetInfo[0], "internal");
+};
+internalButtonHead.onclick = function () {
+  fillInformation(allPlanetInfo[0], "internal");
+};
+
+surfaceGeologyBody.onclick = function () {
+  fillInformation(allPlanetInfo[0], "geology");
+};
+surfaceGeologyHead.onclick = function () {
+  fillInformation(allPlanetInfo[0], "geology");
+};
+
+function fillInformation(planetInfo, type) {
   let planetNameElement = document.getElementsByClassName("planetName")[0];
   let textContent = document.getElementsByClassName("textContent")[0];
   let rotationTime = document
@@ -23,9 +56,18 @@ function fillInformation(planetInfo) {
     .children[3].getElementsByClassName("infoNumber")[0];
   let planetImage = document.getElementsByClassName("mainImg")[0];
 
-  planetImage.src = planetInfo.images.planet;
+  if (type === "overview") {
+    textContent.innerHTML = planetInfo.overview.content;
+    planetImage.src = planetInfo.images.planet;
+  } else if (type === "internal") {
+    textContent.innerHTML = planetInfo.structure.content;
+    planetImage.src = planetInfo.images.internal;
+  } else if (type === "geology") {
+    textContent.innerHTML = planetInfo.geology.content;
+    planetImage.src = planetInfo.images.geology;
+  }
+
   planetNameElement.innerHTML = planetInfo.name;
-  textContent.innerHTML = planetInfo.overview.content;
   rotationTime.innerHTML = planetInfo.rotation;
   revolutionTime.innerHTML = planetInfo.revolution;
   radius.innerHTML = planetInfo.radius;
